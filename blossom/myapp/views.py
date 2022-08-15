225915b6505f876f8b9738a03729aceb4143eb01
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-15 10:53:57 -0500

    added first new page

--- blossom/myapp/views.py
+++ blossom/myapp/views.py
@@ -12,3 +12,6 @@ def index(request):
 def book_by_id(request, book_id):
     book = Book.objects.get(pk=book_id)
     return render(request, 'book_details.html', {'book':book})
+
+def plants_homepage(request):
+    return render(request, 'plants.html')
