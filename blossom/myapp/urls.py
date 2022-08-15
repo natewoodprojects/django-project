f5265602f56d5942b5e7407e0cce8218e084678d
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-15 11:09:02 -0500

    three ish pages that connect to each other

--- blossom/myapp/urls.py
+++ blossom/myapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('book/<int:book_id>', views.book_by_id, name='book_by_id'),
-    path('plants', views.plants_homepage, name='plan')
+    path('plants', views.plants_homepage, name='plants_homepage')
 ]
