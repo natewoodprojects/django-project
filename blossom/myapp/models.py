ef9ccaeb1c1fd35241c88d1c7dc1fce646fb0ec6
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-18 11:50:43 -0500

    started on the database properly

--- blossom/myapp/models.py
+++ blossom/myapp/models.py
@@ -17,4 +17,20 @@ class Plant(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Users(models.Model):
+    email = models.CharField(max_length=200)
+
+
+
+class Plants(models.Model):
+    user_id = models.DecimalField(decimal_places=0, max_digits=10)
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    light = models.CharField(max_length=200)
+    water = models.CharField(max_length=200)
+    humidity = models.CharField(max_length=200)
+    soil = models.CharField(max_length=200)
+    fertilizer = models.CharField(max_length=200)    
+    toxcicity = models.BooleanField()
+    notes = models.CharField(max_length=10000)
