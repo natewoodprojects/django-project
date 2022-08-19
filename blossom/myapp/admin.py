096151029c2b911214611c74d2baa3f3dbc65f92
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-19 11:51:13 -0500

    form mostly complete

--- blossom/myapp/admin.py
+++ blossom/myapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book, Plant
+from .models import Book, Plant, Plants
 admin.site.register(Book)
 admin.site.register(Plant)
+admin.site.register(Plants)
--- blossom/myapp/forms.py
+++ blossom/myapp/forms.py
@@ -3,14 +3,37 @@ from django import forms
 SOIL_CHOICES=[('All-purpose potting mix', 'All-purpose potting mix'), ('Succulent/cactus mix', 'Succulent/cactus mix'), ('Tropical potting mix', 'Tropical potting mix')]
 
 class PlantForm(forms.Form):
-    name = forms.CharField(label='Plant_name', max_length=100)
-    type = forms.CharField(label='Type', max_length=200)
-    light = forms.CharField(label='Light', max_length=200)
-    water = forms.CharField(label='Water', max_length=200)
-    humidity = forms.CharField(label='Humidity', max_length=200)
-    soil = forms.CharField(label='Soil', widget=forms.Select(choices=SOIL_CHOICES))
-    fertilizer = forms.CharField(label='Fertilizer', max_length=200)    
-    toxcicity = forms.BooleanField(label='Toxcicity')
-    notes = forms.CharField(label='Notes', max_length=10000)
+    name = forms.CharField(label='Plant_name', max_length=100, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
+    type = forms.CharField(label='Type', max_length=200, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
+    light = forms.CharField(label='Light', max_length=200, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
+    water = forms.CharField(label='Water', max_length=200, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
+    humidity = forms.CharField(label='Humidity', max_length=200, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
+    soil = forms.CharField(label='Soil',required=False,  widget=forms.Select(choices=SOIL_CHOICES, attrs={
+                               'required': 'True'
+                            }))
+    fertilizer = forms.CharField(label='Fertilizer', max_length=200, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))    
+    toxcicity = forms.BooleanField(label='Toxcicity', required=False)
+    notes = forms.CharField(label='Notes', max_length=10000, required=False, widget=forms.TextInput
+                           (attrs={
+                               'required': 'True'
+                            }))
 
 # 'Aroid soilless potting mix', 'Orchid bark', 'Perlite/vermiculite', 'Coconut coir/peat moss', 'Sphagnum moss', 'LECA', 'Other/custom blend (see notes)']
